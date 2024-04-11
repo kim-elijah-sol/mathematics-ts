ef4b0142f44661ce401cf87a05fccef8c934d014
kim-elijah-sol <kimelijahsol@gmail.com>
2024-04-11 21:41:42 +0900

    지수법칙 2: 지수의 뺄셈

--- src/__test__/indexLawOfPowers.spec.ts
+++ src/__test__/indexLawOfPowers.spec.ts
@@ -1,4 +1,4 @@
-import { lawOfPowers1, lawOfPowers2 } from "../indexLawOfPowers";
+import { lawOfPowers1, lawOfPowers2, lawOfPowers3 } from "../indexLawOfPowers";
 
 describe("지수법칙 1: 지수의 덧셈", () => {
   it("지수법칙 1 : 지수의 덧셈 테스트", () => {
@@ -31,5 +31,22 @@ describe("지수법칙 2: 지수의 곱셈", () => {
   it("지수법칙 2 : 지수의 곱셈 테스트 > 밑이 음수인 경우도 예외는 아님", () => {
     expect(lawOfPowers2(-2, 3, 3)).toBe(-512);
     expect(lawOfPowers2(-2, 3, 3)).toBe((-2) ** (3 * 3));
+  });
+});
+
+describe("지수법칙 3 : 지수의 뺄셈", () => {
+  it("지수법칙 3 : 지수의 뺄셈 테스트", () => {
+    expect(lawOfPowers3(2, 5, 3)).toBe(4);
+    expect(lawOfPowers3(2, 5, 3)).toBe(2 ** 5 / 2 ** 3);
+  });
+
+  it("지수법칙 3 : 지수의 뺄셈 테스트 > 지수가 음수인 경우도 예외는 아님", () => {
+    expect(lawOfPowers3(2, 3, -2)).toBe(2 ** 5);
+    expect(lawOfPowers3(2, 3, -2)).toBe(2 ** 3 / 2 ** -2);
+  });
+
+  it("지수법칙 3 : 지수의 뺄셈 테스트 > 밑이 음수인 경우도 예외는 아님", () => {
+    expect(lawOfPowers3(-2, 5, 2)).toBe(-8);
+    expect(lawOfPowers3(-2, 5, 2)).toBe((-2) ** 5 / (-2) ** 2);
   });
 });
--- src/indexLawOfPowers.ts
+++ src/indexLawOfPowers.ts
@@ -31,3 +31,19 @@ export function lawOfPowers2(
 ): number {
   return base ** (exponent1 * exponent2);
 }
+
+/**
+ * 지수 법칙 3 : 지수의 뺄셈
+ *
+ * @param base
+ * @param exponent1
+ * @param exponent2
+ * a^b / a^c = a^(b-c)
+ */
+export function lawOfPowers3(
+  base: number,
+  exponent1: number,
+  exponent2: number
+): number {
+  return base ** (exponent1 - exponent2);
+}
